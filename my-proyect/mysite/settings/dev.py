3558ca6287994d925077f54fd03de67c5fc86050
AngelZamoranoO <angelzamoranoo@gmail.com>
2024-03-11 18:31:24 -0300

    Primer proyecto de wagtail, proyecto personal

---
+++ my-proyect/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-4gpvp6^y#@1ugmw=1wei5!q&7=b)5j18kgfc2h&n^h_wrvvf@q"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
